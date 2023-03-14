aa4f37d12de9b989c09f940c6d1a265065c14e5b
kelvinkml <kelvinkml@live.com>
2023-03-14 23:31:02 +0000

    adds animejs bg onmouseover

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,66 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import anime from "animejs";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const wrapper = document.getElementById("tiles");
+    let columns = Math.floor(document.body.clientWidth / 50);
+    let rows = Math.floor(document.body.clientHeight / 50);
+
+    const colours = [
+      "rgb(100, 100, 100)",
+      "rgb(255, 255, 255)",
+      "rgb(100, 100, 100)",
+      "rgb(255, 255, 255)",
+      "rgb(100, 100, 100)",
+      "rgb(255, 255, 255)",
+    ];
+    let count = -1;
+
+    const handleOnClick = (index) => {
+      count = count + 1;
+      document.getElementById("tiles").style.zIndex = "0";
+      anime({
+        targets: ".tile",
+        backgroundColor: colours[index % (colours.length - 1)],
+        delay: anime.stagger(100, {
+          grid: [columns, rows],
+          from: index,
+        }),
+      });
+    };
+
+    const createTile = (index) => {
+      const tile = document.createElement("div");
+      tile.classList.add("tile");
+      tile.onmouseover = (e) => handleOnClick(index);
+      return tile;
+    };
+
+    const createTiles = (quantity) => {
+      Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+      });
+    };
+
+    const createGrid = () => {
+      wrapper.innerHTML = "";
+      columns = Math.floor(document.body.clientWidth / 50);
+      rows = Math.floor(document.body.clientHeight / 50);
+      wrapper.style.setProperty("--columns", columns);
+      wrapper.style.setProperty("--rows", rows);
+
+      createTiles(columns * rows);
+    };
+    createGrid();
+    window.onresize = () => createGrid();
+  }, []);
+
+  return (
+    <>
+      <div id="tiles"></div>
+      <Component {...pageProps} />
+    </>
+  );
 }
