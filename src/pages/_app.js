a16d87b343ad2124cd475d92fc83ad2c1c181277
kelvinkml <kelvinkml@live.com>
2023-03-17 17:34:53 +0000

    cleaned up code, removed comments and unused

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,26 +4,9 @@ import anime from "animejs";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    let body = document.body;
-    let html = document.documentElement;
-
-    let height = Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    );
-
-    console.log(height);
-
     const wrapper = document.getElementById("tiles");
-
-    // let columns = Math.floor(window.innerWidth / 50);
-    // let rows = Math.floor(window.innerHeight / 50);
     let columns = Math.ceil(document.body.clientWidth / 50);
     let rows = Math.ceil(document.body.clientHeight / 50);
-    // let rows = Math.ceil(height / 50);
 
     const colours = [
       "rgb(170, 170, 170)",
@@ -62,11 +45,8 @@ export default function App({ Component, pageProps }) {
 
     const createGrid = () => {
       wrapper.innerHTML = "";
-      // columns = Math.ceil(window.innerWidth / 50);
-      // rows = Math.ceil(window.innerHeight / 50);
       columns = Math.ceil(document.body.clientWidth / 50);
       rows = Math.ceil(document.body.clientHeight / 50);
-      // rows = Math.ceil(height / 50);
       wrapper.style.setProperty("--columns", columns);
       wrapper.style.setProperty("--rows", rows);
       console.log(document.body.clientHeight);
