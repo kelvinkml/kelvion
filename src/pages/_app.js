316b6950886ea933ff84b90299284b171c289d59
kelvinkml <kelvinkml@live.com>
2023-03-17 17:07:59 +0000

    moved the tiles down below header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,26 @@ import anime from "animejs";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
+    let body = document.body;
+    let html = document.documentElement;
+
+    let height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+
+    console.log(height);
+
     const wrapper = document.getElementById("tiles");
+
     // let columns = Math.floor(window.innerWidth / 50);
     // let rows = Math.floor(window.innerHeight / 50);
     let columns = Math.floor(document.body.clientWidth / 50);
-    let rows = Math.floor(document.body.clientHeight / 50);
+    // let rows = Math.floor(document.body.clientHeight / 50);
+    let rows = Math.floor(height / 50);
 
     const colours = [
       "rgb(170, 170, 170)",
@@ -50,7 +65,8 @@ export default function App({ Component, pageProps }) {
       // columns = Math.floor(window.innerWidth / 50);
       // rows = Math.floor(window.innerHeight / 50);
       columns = Math.floor(document.body.clientWidth / 50);
-      rows = Math.floor(document.body.clientHeight / 50);
+      // rows = Math.floor(document.body.clientHeight / 50);
+      rows = Math.floor(height / 50);
       wrapper.style.setProperty("--columns", columns);
       wrapper.style.setProperty("--rows", rows);
 
